70dc4fc528b9c5525bb386797caeb5f1bdb62073
Bolaji <bjwealthy@gmail.com>
2020-04-13 09:47:15 +0100

    with shop page and collection prev comp

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,257 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats', 
+        items: [
+            {
+                id: 1,
+                name: 'Browm Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-Shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-Shirt',
+                imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            },
+            
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+            },
+        ]
+    },  
+    
+    
+    {
+        id: 2,
+        title: 'Jackets',
+        items: [
+            {
+                id: 1,
+                name: 'Browm Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-Shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-Shirt',
+                imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            },
+            
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+            },
+        ]
+    },
+
+
+    {
+        id: 3,
+        title: 'Sneakers',
+        items: [
+            {
+                id: 1,
+                name: 'Browm Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-Shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-Shirt',
+                imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            },
+            
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+            },
+        ]
+    },
+
+
+    {
+        id: 4,
+        title: 'Womens',
+        items: [
+            {
+                id: 1,
+                name: 'Browm Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-Shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-Shirt',
+                imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            },
+            
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+            },
+        ]
+    },
+
+
+    {
+        id: 5,
+        title: 'Mens',
+        items: [
+            {
+                id: 1,
+                name: 'Browm Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-Shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-Shirt',
+                imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            },
+            
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+            },
+        ]
+    },
+]
+
+export default SHOP_DATA
